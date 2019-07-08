41bc4f395a73e2438a2ce9997f8c561dad722eac
esim7 <makishev@yahoo.com>
2019-07-08 13:25:06 +0300

    Создан пустой прект

---
+++ homeWork1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //программа "отражает" введенное предложение наоборот
+            var str = "My Name is Esimzhan".ToCharArray();
+            Array.Reverse(str);
+            var res = String.Concat(str);
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
